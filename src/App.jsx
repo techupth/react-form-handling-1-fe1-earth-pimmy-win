10e5db2028c4843733e7d429cb210aad350f9ae2
Palathip <windeesel365@gmail.com>
2024-02-04 20:01:39 +0700

    feat(form_handling):task

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
+import React, { useState } from 'react';
 import "./App.css";
 
 function App() {
+  const [greetingMessage, setGreetingMessage] = useState('Greeting Message');
+  const updateGreeting = () => {
+    const inputElement = document.getElementById('greeting-message');
+    // ตรวจสอบว่า input ไม่เป็น null และมีค่า
+    if (inputElement && inputElement.value.trim() !== '') {
+      setGreetingMessage(inputElement.value);
+    }
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
         <input id="greeting-message" type="text" />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={updateGreeting}>Update text</button>
       </div>
     </div>
   );
